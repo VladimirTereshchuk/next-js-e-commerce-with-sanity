62b1684466d7beb94fa50362fed3b3f6d147cd63
VladimirTereshchuk <vofca1974@gmail.com>
2021-06-13 09:35:13 +0300

    add Cart handleOpen

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Link from "next/link";
+import Cart from "./Cart";
 
 function Layout({ children }) {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -9,11 +10,11 @@ function Layout({ children }) {
   return (
     <div className="bg-white">
       <header>
-        <div className="container mx-auto px-6 py-3">
+        <div className="container px-6 py-3 mx-auto">
           <div className="flex items-center justify-between">
             <div className="hidden w-full text-gray-600 md:flex md:items-center">
               <svg
-                className="h-5 w-5"
+                className="w-5 h-5"
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -33,16 +34,16 @@ function Layout({ children }) {
               </svg>
               <span className="mx-1 text-sm">NY</span>
             </div>
-            <div className="w-full text-gray-700 md:text-center text-2xl font-semibold">
+            <div className="w-full text-2xl font-semibold text-gray-700 md:text-center">
               Pulp Inc.
             </div>
             <div className="flex items-center justify-end w-full">
               <button
                 onClick={handleOpen}
-                className="text-gray-600 focus:outline-none mx-4 sm:mx-0"
+                className="mx-4 text-gray-600 focus:outline-none sm:mx-0"
               >
                 <svg
-                  className="h-5 w-5"
+                  className="w-5 h-5"
                   fill="none"
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -61,7 +62,7 @@ function Layout({ children }) {
                   className="text-gray-600 hover:text-gray-500 focus:outline-none focus:text-gray-500"
                   aria-label="toggle menu"
                 >
-                  <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                  <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
                     <path
                       fillRule="evenodd"
                       d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
@@ -94,10 +95,10 @@ function Layout({ children }) {
               </Link>
             </div>
           </nav>
-          <div className="relative mt-6 max-w-lg mx-auto">
-            <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+          <div className="relative max-w-lg mx-auto mt-6">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
               <svg
-                className="h-5 w-5 text-gray-500"
+                className="w-5 h-5 text-gray-500"
                 viewBox="0 0 24 24"
                 fill="none"
               >
@@ -112,20 +113,17 @@ function Layout({ children }) {
             </span>
 
             <input
-              className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
+              className="w-full py-2 pl-10 pr-4 border rounded-md focus:border-blue-500 focus:outline-none focus:shadow-outline"
               type="text"
               placeholder="Search"
             />
           </div>
         </div>
       </header>
-      {/*
-      // This Cart doesn't really work… yet!
       <Cart cartOpen={cartOpen} handleOpen={handleOpen} />
-      */}
       <main className="my-8">{children}</main>
       <footer className="bg-gray-200">
-        <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+        <div className="container flex items-center justify-between px-6 py-3 mx-auto">
           <a
             href="#"
             className="text-xl font-bold text-gray-500 hover:text-gray-400"
